6b5eb42658b10b6c9e01fac2ea5f7c0bee91c4d9
Ashay Jaiswal <ashaywin@gmail.com>
2022-01-30 17:03:10 +0530

    Add work directory path

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
+const workDir = '/webfiles/'
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World! '+port)
+  res.sendFile('Hello World! '+port)
 })
 
