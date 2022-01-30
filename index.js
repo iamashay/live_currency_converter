685b3130a260940a22a3a57aecc10a8d2da361c0
Ashay Jaiswal <ashaywin@gmail.com>
2022-01-30 17:36:53 +0530

    Include index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
