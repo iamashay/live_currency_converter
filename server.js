bc49341f250b802d19392d8f611ff0b40f735717
Ashay Jaiswal <ashaywin@gmail.com>
2022-01-30 16:33:28 +0530

    Change to http server demo

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+var http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function onRequest(request, response) {
+    response.writeHead(200, {
+        'Content-type': 'text-plain'
+    });
+    response.write('Hello from Node.');
+    response.end();
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// provess.env.PORT will expand to the name pipe value on App Service
+// request --> Frontends (ARR) --> Web Worker (IIS) --> iisnode --> 
+//    --named-pipe--> node.exe server.js
+
+http.createServer(onRequest).listen(process.env.PORT || 3000);
+console.log('Listening for requests on port ' + (process.env.PORT || 3000));
