9c4c27dee79e9ff1be7cde0ec667fd2cd0b6504e
Ashay Jaiswal <ashaywin@gmail.com>
2022-02-13 14:31:23 +0530

    Stage changes

--- webfiles/public/js/script.js
+++ webfiles/public/js/script.js
@@ -1,3 +1,4 @@
+const publicFolder = window.location.href;
 const initialAmount = document.getElementById('initial');
 const initialCurr = document.getElementById('initialCurr');
 const finalCurr = document.getElementById('finalCurr');
@@ -31,15 +32,23 @@ function listSetting(currList){
 listSetting(initialCurr)
 listSetting(finalCurr)
 
-function currentRate(amount, from, to){
+function currentRate(amount, from, to, cb){
+    const equalToSign = document.getElementById('equalto');
+
+    equalToSign.innerHTML = '<img src="'+publicFolder+'loading.gif" alt="loading gif">';
+    console.log(equalToSign.innerHTML)   
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
             jsonData = JSON.parse(xhttp.responseText);
             //console.log( jsonData['rates'][to])
-            return jsonData['rates'][to];
-    }
+            equalToSign.innerHTML = '=';
+
+            cb(jsonData['rates'][to]);
+        }   else {
+        equalToSign.innerHTML = '=';
+        }
     };
     xhttp.open("GET", 'https://api.frankfurter.app/latest?amount='+amount+'&from='+from, true);
     xhttp.send();
@@ -48,25 +57,31 @@ function currentRate(amount, from, to){
 //currentRate(45, 'INR', 'USD')
 
 function UpdateRate() {
+    
     if (initialAmount.value 
-    || typeof initialAmount.value === 'number'
-    || initialCurr.value !== '' 
-    || finalCurr.value !== '')
+    && initialCurr.value !== '' 
+    && finalCurr.value !== '')
     {
-        finalAmount.value = currentRate(initialAmount.value, initialCurr.value, finalCurr.value);
-    }else if (typeof initialAmount.value === 'number' && initialAmount.value === 0){
+        currentRate(initialAmount.value, initialCurr.value, finalCurr.value, (newAmount) => {
+            finalAmount.value = newAmount 
+        });
+        
+    }else if (initialAmount.value == 0){
         finalAmount.value = 0;
     }else {
         alert('Some error occured!')
     }
 }
 
-function autoListener(items){
-    items.addEventListener('change', ()=>{
-        UpdateRate()
-    })
-}
 
-autoListener(initialAmount)
-autoListener(initialCurr)
-autoListener(finalCurr)
+
+initialAmount.addEventListener('keyup', ()=>{
+    UpdateRate()
+})
+initialCurr.addEventListener('change', ()=>{
+    UpdateRate()
+})
+finalCurr.addEventListener('change', ()=>{
+    UpdateRate()
+})
+
